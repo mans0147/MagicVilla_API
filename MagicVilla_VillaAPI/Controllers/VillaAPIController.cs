f16ce802bdb46eb0277f70eba828fab4f1ec5733
mans <hamza147amr@gmail.com>
2023-12-11 20:04:53 +0200

    Add villa controller get all , get by id Add Models villa model Add DTO villaDTO

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetAllVillas() 
+        {
+            return VillaStore.VillaList;
+        }
+
+        [HttpGet("{id:int}")] 
+        public VillaDTO GetAllVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
